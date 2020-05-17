708650ced7936328cc3e0bca31b32bde2305ba84
sandaramet <aram@gratun.am>
2020-05-18 01:55:49 +0400

    redirect page

--- front-end/src/app/pages/home/signup/signup.component.ts
+++ front-end/src/app/pages/home/signup/signup.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { AuthService } from "src/app/serivces/auth.service";
+import { NewUser } from "src/app/models/NewUser.model";
 @Component({
   selector: "app-signup",
   templateUrl: "./signup.component.html",
   styleUrls: ["./signup.component.scss"],
 })
 export class SignupComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private auth: AuthService) {}
+  user: NewUser = {
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  };
   onSubmit() {
-    this.router.navigateByUrl('/home/login')
+    console.log(this.user)
+    this.auth.registerNewUser(this.user);
+    // this.router.navigateByUrl("/home/login");
   }
   ngOnInit() {}
 }
