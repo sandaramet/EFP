beebb7e4d159145c2efc73e8ad93ed5269563d7d
sandaramet <aram@gratun.am>
2020-05-17 22:39:50 +0400

    create components module

---
+++ front-end/src/app/components/components.module.ts
@@ -0,0 +1,7 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+@NgModule({
+  declarations: [],
+  exports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class ComponentsModule {}
