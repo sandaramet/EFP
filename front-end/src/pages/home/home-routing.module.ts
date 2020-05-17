ce7a26fbec23ff772c830784e4ca84830d192a70
sandaramet <aram@gratun.am>
2020-05-17 23:15:51 +0400

    display only one login | signup

--- front-end/src/pages/home/home-routing.module.ts
+++ front-end/src/pages/home/home-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
         component: SignupComponent,
       },
       {
+        path: "signup",
+        redirectTo: "",
+      },
+      {
         path: "login",
         component: LoginComponent,
       },
