61f83ae6925e20e7e2399fac95fe6c027eec0b22
sandaramet <aram@gratun.am>
2020-05-17 22:48:09 +0400

    header: @input and ng-content

--- front-end/src/app/components/header/header.component.ts
+++ front-end/src/app/components/header/header.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  @Input() title: string;
+  constructor() {}
 
   ngOnInit() {}
-
 }
