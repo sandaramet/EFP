69a1ed8bfe5046018ce21a06520652ff2f3b8566
sandaramet <aram@gratun.am>
2020-05-18 17:23:32 +0400

    fix canActivate()

--- front-end/src/app/guards/auth-guard.service.ts
+++ front-end/src/app/guards/auth-guard.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
-import { AuthService } from '../serivces/auth.service';
+import { Injectable } from "@angular/core";
+import { Router, CanActivate } from "@angular/router";
+import { AuthService } from "../serivces/auth.service";
 @Injectable()
 export class AuthGuardService implements CanActivate {
   constructor(public auth: AuthService, public router: Router) {}
   canActivate(): boolean {
-    if (!this.auth.registerNewUser) {
-      this.router.navigate(['login']);
+    if (!this.auth.isLoggedIn) {
+      this.router.navigate(["home"]);
       return false;
     }
     return true;
