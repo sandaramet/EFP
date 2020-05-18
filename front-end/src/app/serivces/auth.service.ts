ae7b8a0f3e164016e3a930fa69e768c583c3bb55
sandaramet <aram@gratun.am>
2020-05-18 23:01:54 +0400

    connect check functions and add alerts

--- front-end/src/app/serivces/auth.service.ts
+++ front-end/src/app/serivces/auth.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from "@angular/core";
 import { NewUser } from "../models/NewUser.model";
 import { User } from "../models/User.model";
 import { Router } from "@angular/router";
+import { CheckFormService } from "./check-form.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
   private loggedInStatus: boolean = false;
-  constructor(private router: Router) {}
+  constructor(private router: Router, private checkForm: CheckFormService) {}
   registerNewUser(user: NewUser) {
-    console.log(user.email, user.username, user.confirmPassword, user.password);
+  
+
+    this.router.navigateByUrl("/home/login");
   }
   setLoginStatus(value: boolean) {
     this.loggedInStatus = value;
@@ -19,9 +22,11 @@ export class AuthService {
     return this.loggedInStatus;
   }
   login(user: User) {
-    if (user.username == "admin" && user.password == "admin") {
-      this.router.navigateByUrl("/profile");
-      this.setLoginStatus(true);
-    } else this.setLoginStatus(false);
+    // if (user.username == "admin" && user.password == "admin") {
+    //   this.router.navigateByUrl("/profile");
+    //   this.setLoginStatus(true);
+    // } else {
+    //   this.setLoginStatus(false);
+    // }
   }
 }
