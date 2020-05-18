c6de8677b47afd82c7f834ac66edfea97f2593fa
sandaramet <aram@gratun.am>
2020-05-18 15:17:51 +0400

    login : add User model

--- front-end/src/app/pages/home/login/login.component.ts
+++ front-end/src/app/pages/home/login/login.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/models/User.model";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  user: User = {
+    username: "",
+    password: "",
+  };
+  constructor() {}
 
   ngOnInit() {}
-
 }
