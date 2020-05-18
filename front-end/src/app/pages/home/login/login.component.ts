676ede348605a19912d0adeabe1eb2a6e62c53e6
sandaramet <aram@gratun.am>
2020-05-18 23:05:47 +0400

    delete unnesesary codes

--- front-end/src/app/pages/home/login/login.component.ts
+++ front-end/src/app/pages/home/login/login.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 import { User } from "src/app/models/User.model";
 import { AuthService } from "src/app/serivces/auth.service";
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  constructor(private auth: AuthService, private router: Router) {}
   user: User = {
     username: "",
     password: "",
   };
+  constructor(private auth: AuthService,private router:Router) {}
 
   ngOnInit() {}
   login() {
     this.auth.login(this.user);
+
   }
 }
