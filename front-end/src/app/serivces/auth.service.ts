0addb04660008f2651d00400a9c5ec6aa1966aca
sandaramet <aram@gratun.am>
2020-05-18 15:22:01 +0400

    auth: create login()

--- front-end/src/app/serivces/auth.service.ts
+++ front-end/src/app/serivces/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { NewUser } from "../models/NewUser.model";
+import { User } from "../models/User.model";
 
 @Injectable({
   providedIn: "root",
@@ -8,5 +9,8 @@ export class AuthService {
   constructor() {}
   registerNewUser(user: NewUser) {
     console.log(user.email, user.username, user.confirmPassword, user.password);
+  }
+  login(user: User) {
+    console.log(user.username, user.password);
   }
 }
