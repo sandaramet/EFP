69647a1b699adebe73b2f4d4fbd33f5b25a35804
sandaramet <aram@gratun.am>
2020-05-18 17:25:31 +0400

    add setLoginStatus() and isLoggedIn

--- front-end/src/app/serivces/auth.service.ts
+++ front-end/src/app/serivces/auth.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from "@angular/core";
 import { NewUser } from "../models/NewUser.model";
 import { User } from "../models/User.model";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  constructor() {}
+  private loggedInStatus: boolean = false;
+  constructor(private router: Router) {}
   registerNewUser(user: NewUser) {
     console.log(user.email, user.username, user.confirmPassword, user.password);
   }
+  setLoginStatus(value: boolean) {
+    this.loggedInStatus = value;
+  }
+  get isLoggedIn() {
+    return this.loggedInStatus;
+  }
   login(user: User) {
-    console.log(user.username, user.password);
+    if (user.username == "admin" && user.password == "admin") {
+      this.router.navigateByUrl("/profile");
+      this.setLoginStatus(true);
+    } else this.setLoginStatus(false);
   }
 }
