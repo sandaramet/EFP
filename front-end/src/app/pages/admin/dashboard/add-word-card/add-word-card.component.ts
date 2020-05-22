0790a78549cbb887cc13aed6e38a90e311eac64a
sandaramet <aram@gratun.am>
2020-05-22 21:36:11 +0400

    add alert message

--- front-end/src/app/pages/admin/dashboard/add-word-card/add-word-card.component.ts
+++ front-end/src/app/pages/admin/dashboard/add-word-card/add-word-card.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AlertController } from "@ionic/angular";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-add-word-card',
-  templateUrl: './add-word-card.component.html',
-  styleUrls: ['./add-word-card.component.scss'],
+  selector: "app-add-word-card",
+  templateUrl: "./add-word-card.component.html",
+  styleUrls: ["./add-word-card.component.scss"],
 })
 export class AddWordCardComponent implements OnInit {
+  constructor(
+    public alertController: AlertController,
+    private router: Router
+  ) {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: "Դուք ավելացրիք նոր բառ",
+      message: "Գնալ բառերի աղյուսակ",
+      buttons: [
+        {
+          text: "Գնալ",
+          handler: () => {
+            this.router.navigateByUrl("/admin/dashboard");
+          },
+        },
+      ],
+    });
 
-  constructor() { }
-
+    await alert.present();
+  }
+  addNewWordCard(){
+    this.presentAlert()
+  }
   ngOnInit() {}
-
 }
