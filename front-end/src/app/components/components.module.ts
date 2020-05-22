7461a47f757f51b4164fc73399080732afdc12bd
sandaramet <aram@gratun.am>
2020-05-22 22:33:32 +0400

    feed component configs

--- front-end/src/app/components/components.module.ts
+++ front-end/src/app/components/components.module.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from "./header/header.component";
 import { WordCardComponent } from "./word-card/word-card.component";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
@@ -10,12 +11,14 @@ import { SignupComponent } from "./signup/signup.component";
     WordCardComponent,
     LoginComponent,
     SignupComponent,
+    FeedComponent
   ],
   exports: [
     HeaderComponent,
     WordCardComponent,
     LoginComponent,
     SignupComponent,
+    FeedComponent
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
